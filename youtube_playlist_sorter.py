42f220ae9c2a9852d9797859d5973c49c2fe699b
Malik-Talha <realmaliktalha@gmail.com>
2023-03-04 01:44:50 +0500

    finish

---
+++ youtube_playlist_sorter.py
@@ -0,0 +1,58 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+
+def getResponse(driver, url):
+	print("Getting response. Please wait.......")
+	driver.get(url)
+	driver.implicitly_wait(20)
+	return driver
+
+def sort_videos(driver, path):
+	directory_list = os.listdir(path)
+	names = driver.find_elements_by_id("video-title")
+	print("Sorting. Please wait.......")
+	for i, new_name in enumerate(names):
+		# if new_name.text in directory_list:
+		for index, item in enumerate(directory_list):
+			#if the file is already sorted
+			if item[0] == i:
+				break 
+			# replacing all other characters into '_'
+			translation_table = str.maketrans("&\",/\\?|#$%'", "___________")
+			if new_name.text.strip().translate(translation_table) in item:
+				new_file_name = os.path.join(path, str(1+i) + ' ' + str(new_name.text.strip().translate(translation_table)) + '.mp4')
+				os.rename(os.path.join(path, directory_list[index]), new_file_name)
+				break
+
+
+def main():
+	#setting the new path for webdriver
+	new_directory = 'F:/__Mix Soft 1'
+	current_path = os.environ["PATH"]
+	os.environ["PATH"] = new_directory + ";" + current_path
+
+	#making chrome headless
+	options = webdriver.ChromeOptions()
+	options.add_argument('--headless')
+	# options.add_argument('excludeswitches','[enable-automation]')
+	
+	driver = webdriver.Chrome(options=options)
+
+	# getting playlist url from the user
+	url = input("Enter the url of Youtube Playlist: ")
+
+	driver = getResponse(driver,url)
+
+	# getting folder path of the folder to be sorted in
+	path = input("Enter the path of folder: ").replace('\\','/')
+	sort_videos(driver, path)
+	driver.quit()
+	print("Success!")
+
+if __name__ == "__main__":
+	main()
+
+
+        
